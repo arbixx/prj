1fe52eb9c890da13a7bb3e8bbe5db636c012aa4f
OpenShift guest <arbixy@gmail.com>
2013-04-15 12:08:53 -0400

    UserAgent class modified and added in layout

--- public/js/custom/CUserAgent.class.js
+++ public/js/custom/CUserAgent.class.js
@@ -9,6 +9,39 @@ function CUserAgent() {
     var szUserAgent = null;
     
     
+    this.isAndroid = function(){
+        if ( szUserAgent !== null && typeof(szUserAgent) != 'undefined'){
+            if (szUserAgent.indexOf("android") > -1)
+                return true;
+        }
+        else{
+            //TODO: handle this case
+        }
+        return false;
+    }
+    
+    this.isIphone = function(){
+        if ( szUserAgent !== null && typeof(szUserAgent) != 'undefined'){
+            if (szUserAgent.indexOf("iphone") > -1)
+                return true;
+        }
+        else{
+            //TODO: handle this case
+        }
+        return false;
+    }
+    
+    this.isIpad = function(){
+        if ( szUserAgent !== null && typeof(szUserAgent) != 'undefined'){
+            if (szUserAgent.indexOf("ipad") > -1)
+                return true;
+        }
+        else{
+            //TODO: handle this case
+        }
+        return false;
+    }
+    
     
     /** Getters and Setters */
     this.getUserAgent = function(){
@@ -16,7 +49,7 @@ function CUserAgent() {
     }
     
     this.setUserAgent = function(szUA){
-        szUserAgent = szUA;
+        szUserAgent = szUA.toLowerCase();
     }
     
 }
